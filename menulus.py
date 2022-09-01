af64f6e5f0892d5d26845eaae18e3669cc595e09
Javier <Cadetmunoz@gmail.com>
2022-08-31 22:05:01 -0400

    init commit

---
+++ menulus.py
@@ -0,0 +1,33 @@
+#Komodo Miami Asian Resturant
+
+#     https://www.komodomiami.com/menu/
+
+#Da Teng Zhen Wei
+
+#     https://datangzhenwei.com/wp-content/uploads/2021/10/Da-Tang-Zhen-Wei-Main-Menu.pdf
+
+#Fuchai Miami
+
+#     https://www.grubhub.com/restaurant/fuchai-2506-ne-2nd-ave-miami/560420
+
+#Hutong Miami
+
+#    https://static1.squarespace.com/static/5d7a310ecf6a836b130fcd09/t/62ab432ec556ff153ebbaf47/1655391022706/BUSINESS+%26+SIGNATURE+MENUS+June+2022.pdf
+
+#((Hutong has different menu's for different items))
+
+#Yip Dip Sun
+
+#        https://menu.thedoralyard.com/yip?menu=62c18f50-fe94-4863-bf42-8b39a22a433d
+
+#Sushi Shima
+
+#           https://www.shimamiami.com/_files/ugd/80a93b_63a5a60d41884a53863f180527b821b2.pdf
+
+#Chow One Korean Steakhouse
+
+#   http://www.chowonekoreansteakhouse.com/menu/
+
+#Tanuki Miami
+
+#   https://www.tanukimiami.com/menus/
